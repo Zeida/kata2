77fef750964578e41003e86387b8b1c47c8de743
45395507 <45395507@DESKTOP-3D5M5VE>
2019-01-10 01:35:28 +0000

    kata2 version1

--- NetBeansProjects/IS2 PL2/kata2/src/kata2/Kata2.java
+++ NetBeansProjects/IS2 PL2/kata2/src/kata2/Kata2.java
@@ -1,5 +1,6 @@
 package kata2;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,15 +8,16 @@ public class Kata2 {
 
     public static void main(String[] args) {
         int[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};         
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>(); 
-
-
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])) histogram.put(data[i], histogram.get(data[i])+1); 
-            else histogram.put(data[i], 1); 
+        Map<Integer, Integer> histogram = new HashMap<>(); 
+ 
+        for (int value : data) {
+            histogram.put(value, histogram.containsKey(value) ? histogram.get(value)+ 1:1);
         }
+
         histogram.keySet().forEach((key) -> {
             System.out.println(key + " --> " + histogram.get(key));
         });
+
+        System.out.println(Arrays.asList(histogram));
     }
 }
