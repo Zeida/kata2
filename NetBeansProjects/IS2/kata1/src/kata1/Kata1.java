1d9b68b897fcd67bf9fe136c507449976885bed6
45395507 <45395507@DESKTOP-3D5M5VE>
2019-01-10 00:37:51 +0000

    kata1 version1

--- NetBeansProjects/IS2/kata1/src/kata1/Kata1.java
+++ NetBeansProjects/IS2/kata1/src/kata1/Kata1.java
@@ -1,16 +1,10 @@
 package kata1;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 public class Kata1 {
     
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1997,0,5);
-        
-        Person person= new Person("Zeida", date);
-        System.out.println(person.getName() + " tiene " + person.getAge()+" años");
+        Person person = new Person("Zeida", new Date(97,0,5));
+        System.out.println(person.getName() + " tiene " + person.getAge());
     }
     
 }
